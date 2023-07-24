6874932fc184fecb223fa6b07c1a81ac014f5c7c
Bogdan Despotov <despotov@gmail.com>
2023-07-24 11:35:49 +0300

    Adding an option to chunk the text

--- convert_to_jsonl.py
+++ convert_to_jsonl.py
@@ -7,12 +7,16 @@ import os
 import json
 import click
 
+from typing import List
+
 
 @click.command()
 @click.option('--input-dir', '-i', required=True, type=str)
 @click.option('--output-file', '-o', required=True, type=str)
 @click.option('--split', '-s', required=False, type=int, default=None)
-def convert_to_jsonl(input_dir: str, output_file: str, split: int) -> None:
+@click.option('--chunk-size', '-c', required=False, type=int, default=1680)
+def convert_to_jsonl(input_dir: str, output_file: str,
+                     split: int, chunk_size: int) -> None:
     counter = 0
     with open(output_file, 'w', encoding='utf8') as output:
         for root, dirs, files in os.walk(input_dir):
@@ -21,14 +25,37 @@ def convert_to_jsonl(input_dir: str, output_file: str, split: int) -> None:
                     counter += 1
                     if split is not None and counter > split:
                         break
+                    print(f"Processing file {file} in {root}")
                     with open(os.path.join(root, file),
                               'r', encoding='utf8') as input_file:
                         data = input_file.read().replace('\t', '')
-                        json.dump({'text': data}, output,
-                                  ensure_ascii=False)
-                        output.write('\n')
+                        text_chunks = chunk_text(data, chunk_size)
+                        print(f"Writing {len(text_chunks)} chunks to file")
+                        for chunk in text_chunks:
+                            json.dump({'text': chunk.lstrip()}, output,
+                                      ensure_ascii=False)
+                            output.write('\n')
     click.echo(f'Finished converting {counter} '
                f'files from {input_dir} to {output_file}')
+
+
+def chunk_text(text: str, max_length: int) -> List[str]:
+    # Split text into chunks of max_length until the last symbol is a newline
+    chunks = []
+    while len(text) > max_length:
+        print(f"Remaining text length: {len(text)}")
+        last_stop = text[:max_length].rfind('.')
+        while last_stop == -1:
+            last_stop = text[:max_length].rfind(' ')
+            if last_stop != -1:
+                break
+            else:
+                max_length += 1000
+        last_stop += 1
+        chunks.append(text[:last_stop])
+        text = text[last_stop:]
+    chunks.append(text)
+    return chunks
 
 
 if __name__ == '__main__':
